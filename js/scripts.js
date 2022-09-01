1e108430cc38597fb52e8effd390fbb46e4706e0
RemyXVX <remyfranciscoflores@gmail.com>
2022-08-31 18:55:38 -0700

    Adding README so I can test the code

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,17 @@ function numberOfOccurrencesInText(word, text) {
   });
   return wordCount;
 }
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
