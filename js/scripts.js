b43bbcffebeb2e0deadbdda6cab52d49fe36ab72
RemyXVX <remyfranciscoflores@gmail.com>
2022-08-31 19:51:16 -0700

    Did a couple of more text that helps seperate word and text and add elements to emph them

--- js/scripts.js
+++ js/scripts.js
@@ -2,29 +2,50 @@
 function wordCounter(text) {
   if (text.trim().length === 0) {
     return 0;
-  }
+  };
   let wordCount = 0;
   const textArray = text.split(" ");
   textArray.forEach(function(element) {
     if (!Number(element)) {
       wordCount++;
-    }
+    };
   });
   return wordCount;
-}
+};
 
 function numberOfOccurrencesInText(word, text) {
   if (word.trim().length === 0) {
     return 0;
-  }
+  };
   const textArray = text.split(" ");
   let wordCount = 0;
   textArray.forEach(function(element) {
     if (element.toLowerCase().includes(word.toLowerCase())) {
       wordCount++;
-    }
+    };
   });
   return wordCount;
+}
+
+function boldPassage(word, text) {
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+    return null;
+  }
+  const p = document.createElement("p");
+  let textArray = text.split(" ");
+  textArray.forEach(function(element) {
+    if (word === element) {
+      const bold = document.createElement("strong");
+      bold.append(element);
+      p.append(bold);
+    } else {
+      p.append(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      p.append(" ");
+    };
+  });
+  return p;
 }
 
 function handleFormSubmission(event) {
@@ -35,7 +56,7 @@ function handleFormSubmission(event) {
   const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
   document.getElementById("total-count").innerText = wordCount;
   document.getElementById("selected-count").innerText = occurrencesOfWord;
-}
+};
 
 window.addEventListener("load", function() {
   document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
