c0f47d7b1d75561a7f9ec702f54c3ac8b967f7b2
RemyXVX <remyfranciscoflores@gmail.com>
2022-08-31 19:08:38 -0700

    Tested out our code to see if it will recognized whitespace as empty and return 0

---
+++ README.md
@@ -0,0 +1,6 @@
+Test: "If an empty string is passed in as a word, it should return 0."
+Code:
+const word = "";
+const text = "red RED Red!";
+numberOfOccurrencesInText(word, text);
+Expected Output: 0
--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,9 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
+  if (word.trim().length === 0) {
+    return 0;
+  }
   const textArray = text.split(" ");
   let wordCount = 0;
   textArray.forEach(function(element) {
